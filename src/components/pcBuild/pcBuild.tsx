c5a581682df613edda81554abbfabce328a05632
rjsarellano <rjsarellano@gmail.com>
2025-06-30 23:57:13 +0800

    added pages

---
+++ src/components/pcBuild/pcBuild.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const pcParts = {};
+
+const pcBuild = () => {
+  return (
+    <div>
+      <div>
+        <button type="button">Create Build</button>
+        <button type="button">Recommended Builds</button>
+      </div>
+    </div>
+  );
+};
+
+export default pcBuild;
