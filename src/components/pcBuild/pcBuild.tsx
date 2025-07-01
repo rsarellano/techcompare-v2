f050b73d4c40ab9bbd9b455b14414b969c631f35
rjsarellano <rjsarellano@gmail.com>
2025-07-01 22:28:02 +0800

    added pcBuild

--- src/components/pcBuild/pcBuild.tsx
+++ src/components/pcBuild/pcBuild.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 const pcParts = {};
 
 const pcBuild = () => {
+  const [clicked, isClicked] = useState(0);
+
   return (
-    <div>
-      <div>
-        <button type="button">Create Build</button>
-        <button type="button">Recommended Builds</button>
+    <div className="rounded border border-black h-[550px] w-[800px]">
+      <div className="flex justify-center mt-[80px]">
+        <button
+          type="button"
+          className="bg-sky-500 hover:bg-sky-700 rounded cursor-pointer m-5"
+        >
+          Create Build
+        </button>
+        <button
+          type="button"
+          className="bg-sky-500 hover:bg-sky-700 rounded cursor-pointer m-5"
+        >
+          Recommended Builds
+        </button>
       </div>
     </div>
   );
